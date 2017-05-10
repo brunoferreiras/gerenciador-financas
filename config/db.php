6d0726ca68d69c30411c9819c0f0b8ba23e12446
Bruno Ferreira <fs.brunoferreira@gmail.com>
2017-05-10 11:54:59 -0300

    Configurações iniciais e instalado phinx

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'development' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'gerenciador_financeiro',
+        'username' => 'root',
+        'password' => 'root',
+        'charset' => 'utf-8',
+        'collation' => 'utf8-unicode_ci'
+    ]
+];
