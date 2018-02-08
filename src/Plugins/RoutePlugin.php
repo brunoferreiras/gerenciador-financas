1ea5db05b46897623bb7f7a7fd35b02060ce69ef
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-02-08 11:18:12 -0300

    Configura combinador de rotas

--- src/Plugins/RoutePlugin.php
+++ src/Plugins/RoutePlugin.php
@@ -1,10 +1,14 @@
 <?php
+declare(strict_types=1);
 
 namespace Fin\Plugins;
 
 
 use Aura\Router\RouterContainer;
 use Fin\ServiceContainerInterface;
+use Interop\Container\ContainerInterface;
+use Psr\Http\Message\RequestInterface;
+use Zend\Diactoros\ServerRequestFactory;
 
 class RoutePlugin implements PluginInterface
 {
@@ -18,9 +22,24 @@ class RoutePlugin implements PluginInterface
         $matcher = $routerContainer->getMatcher();
         /* Tem a função de gerar links com base nas rotas registradas */
         $generator = $routerContainer->getGenerator();
+        $request = $this->getRequest();
 
         $container->add('routing', $map);
         $container->add('routing.matcher', $matcher);
         $container->add('routing.generator', $generator);
+        $container->add(RequestInterface::class, $request);
+
+        $container->addLazy('route', function (ContainerInterface $container) {
+            $matcher = $container->get('routing.matcher');
+            $request = $container->get(RequestInterface::class);
+            return $matcher->match($request);
+        });
+    }
+
+    protected function getRequest(): RequestInterface
+    {
+        return ServerRequestFactory::fromGlobals(
+            $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+        );
     }
 }
